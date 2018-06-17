984628783fd0c4ede8c91a40624365b5e6075c57
Anna Garcia <annaegarcia@gmail.com>
2018-06-16 20:09:53 -0400

    refactor CardObject

--- Set/CardObject.swift
+++ Set/CardObject.swift
@@ -13,33 +13,15 @@ class CardObject: UIView {
     private var symbol: Card.Symbol?
     private var shading: Card.Shading?
     private var color: Card.Color?
-    
     private var objectPath: UIBezierPath? {
         if let symbol = symbol {
             switch symbol {
             case .oval:
-                return UIBezierPath(roundedRect: bounds, cornerRadius: 128)
+                return drawOval()
             case .diamond:
-                let path = UIBezierPath()
-                path.move(to: CGPoint(x: bounds.midX, y: bounds.minY))
-                path.addLine(to: CGPoint(x: bounds.minX, y: bounds.height / 2))
-                path.addLine(to: CGPoint(x: bounds.midX, y: bounds.maxY))
-                path.addLine(to: CGPoint(x:bounds.maxX, y: bounds.height / 2))
-                path.close()
-                return path
+                return drawDiamond()
             case .squiggle:
-                let bezierPath = UIBezierPath()
-                bezierPath.move(to: CGPoint(x: bounds.maxX * 0.10, y: bounds.maxY))
-                bezierPath.addCurve(to: CGPoint(x: bounds.minX, y: bounds.maxY * 0.80), controlPoint1: CGPoint(x: bounds.minX, y: bounds.maxY), controlPoint2: CGPoint(x: bounds.minX, y: bounds.maxY * 0.85))
-                bezierPath.addCurve(to: CGPoint(x: bounds.maxX * 0.30, y: bounds.minY), controlPoint1: CGPoint(x: bounds.minX, y: bounds.midY), controlPoint2: CGPoint(x: bounds.maxX * 0.11, y: bounds.minY))
-                bezierPath.addCurve(to: CGPoint(x: bounds.maxX * 0.65, y: bounds.maxY * 0.30), controlPoint1: CGPoint(x: bounds.midX, y: bounds.minY), controlPoint2: CGPoint(x: bounds.midX, y: bounds.midY))
-                bezierPath.addCurve(to: CGPoint(x: bounds.maxX * 0.90, y: bounds.minY), controlPoint1: CGPoint(x: bounds.maxX * 0.70, y: bounds.midY * 0.5), controlPoint2: CGPoint(x: bounds.maxX * 0.85, y: bounds.minY))
-                bezierPath.addCurve(to: CGPoint(x: bounds.maxX * 0.88, y: bounds.maxY * 0.88), controlPoint1: CGPoint(x: bounds.maxX * 1.10, y: bounds.minY), controlPoint2: CGPoint(x: bounds.maxX * 0.95, y: bounds.maxY * 0.80))
-                bezierPath.addCurve(to: CGPoint(x: bounds.maxX * 0.60, y: bounds.maxY * 0.90), controlPoint1: CGPoint(x: bounds.maxX * 0.80, y: bounds.maxY), controlPoint2: CGPoint(x: bounds.maxX * 0.75, y: bounds.maxY))
-                bezierPath.addCurve(to: CGPoint(x: bounds.maxX * 0.40, y: bounds.maxY * 0.75), controlPoint1: CGPoint(x: bounds.midX, y: bounds.maxY * 0.80), controlPoint2: CGPoint(x: bounds.midX, y: bounds.maxY * 0.80))
-                bezierPath.addCurve(to: CGPoint(x: bounds.maxX * 0.10, y: bounds.maxY), controlPoint1: CGPoint(x: bounds.maxX * 0.30, y: bounds.maxY * 0.75), controlPoint2: CGPoint(x: bounds.maxX * 0.20, y: bounds.maxY))
-                bezierPath.close()
-                return bezierPath
+                return drawSquiggle()
             }
         }
         return nil
@@ -54,38 +36,76 @@ class CardObject: UIView {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
     }
     
     override func draw(_ rect: CGRect) {
         if let shape = objectPath, let shading = shading, let cardColor = color {
+            // clip shape to add shading and color
             shape.addClip()
+            // get color from Card color enum
             let color = getColor(for: cardColor)
-            
+            // shade shape with color
             switch shading {
             case .open:
-                setOutline(color: color, shape: shape)
+                drawOutline(color: color, shape: shape)
             case .solid:
-                color.setFill()
-                shape.fill()
+                fillWith(color: color, for: shape)
             case .striped:
-                setOutline(color: color, shape: shape)
-                for x in stride(from: 0, to: bounds.width * 2, by: bounds.width / 40) {
-                    let path = UIBezierPath()
-                    path.move(to: CGPoint(x: x, y: 0))
-                    path.addLine(to: CGPoint(x: 0, y: x))
-                    path.stroke()
-                }
+                drawOutline(color: color, shape: shape)
+                drawStripes()
             }
         }
     }
     
-    private func setOutline(color: UIColor, shape: UIBezierPath) {
+    // Draw Shapes
+    private func drawOval() -> UIBezierPath {
+        return UIBezierPath(roundedRect: bounds, cornerRadius: 128)
+    }
+    private func drawDiamond() -> UIBezierPath {
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: bounds.midX, y: bounds.minY))
+        path.addLine(to: CGPoint(x: bounds.minX, y: bounds.height / 2))
+        path.addLine(to: CGPoint(x: bounds.midX, y: bounds.maxY))
+        path.addLine(to: CGPoint(x:bounds.maxX, y: bounds.height / 2))
+        path.close()
+        return path
+    }
+    private func drawSquiggle() -> UIBezierPath {
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: bounds.maxX * 0.10, y: bounds.maxY))
+        path.addCurve(to: CGPoint(x: bounds.minX, y: bounds.maxY * 0.80), controlPoint1: CGPoint(x: bounds.minX, y: bounds.maxY), controlPoint2: CGPoint(x: bounds.minX, y: bounds.maxY * 0.85))
+        path.addCurve(to: CGPoint(x: bounds.maxX * 0.30, y: bounds.minY), controlPoint1: CGPoint(x: bounds.minX, y: bounds.midY), controlPoint2: CGPoint(x: bounds.maxX * 0.11, y: bounds.minY))
+        path.addCurve(to: CGPoint(x: bounds.maxX * 0.65, y: bounds.maxY * 0.30), controlPoint1: CGPoint(x: bounds.midX, y: bounds.minY), controlPoint2: CGPoint(x: bounds.midX, y: bounds.midY))
+        path.addCurve(to: CGPoint(x: bounds.maxX * 0.90, y: bounds.minY), controlPoint1: CGPoint(x: bounds.maxX * 0.70, y: bounds.midY * 0.5), controlPoint2: CGPoint(x: bounds.maxX * 0.85, y: bounds.minY))
+        path.addCurve(to: CGPoint(x: bounds.maxX * 0.88, y: bounds.maxY * 0.88), controlPoint1: CGPoint(x: bounds.maxX * 1.10, y: bounds.minY), controlPoint2: CGPoint(x: bounds.maxX * 0.95, y: bounds.maxY * 0.80))
+        path.addCurve(to: CGPoint(x: bounds.maxX * 0.60, y: bounds.maxY * 0.90), controlPoint1: CGPoint(x: bounds.maxX * 0.80, y: bounds.maxY), controlPoint2: CGPoint(x: bounds.maxX * 0.75, y: bounds.maxY))
+        path.addCurve(to: CGPoint(x: bounds.maxX * 0.40, y: bounds.maxY * 0.75), controlPoint1: CGPoint(x: bounds.midX, y: bounds.maxY * 0.80), controlPoint2: CGPoint(x: bounds.midX, y: bounds.maxY * 0.80))
+        path.addCurve(to: CGPoint(x: bounds.maxX * 0.10, y: bounds.maxY), controlPoint1: CGPoint(x: bounds.maxX * 0.30, y: bounds.maxY * 0.75), controlPoint2: CGPoint(x: bounds.maxX * 0.20, y: bounds.maxY))
+        path.close()
+        return path
+    }
+    
+    // Draw Shading
+    private func fillWith(color: UIColor, for shape: UIBezierPath){
+        color.setFill()
+        shape.fill()
+    }
+    private func drawStripes() {
+        for x in stride(from: 0, to: bounds.width * 2, by: bounds.width / 40) {
+            let path = UIBezierPath()
+            path.move(to: CGPoint(x: x, y: 0))
+            path.addLine(to: CGPoint(x: 0, y: x))
+            path.stroke()
+        }
+    }
+    private func drawOutline(color: UIColor, shape: UIBezierPath) {
         color.setStroke()
         shape.lineWidth = bounds.width / 60
         shape.stroke()
     }
     
+    // Get Color
     private func getColor(for color:Card.Color) -> UIColor {
         switch color {
         case .teal:
